16222bde041ffb974269e90fe1490c68a4ad1ffe
bruhmeboii <dak3rmann@gmail.com>
2024-10-31 09:21:24 +0100

    Funciones1 Terminadas

--- Reto2/src/Funciones1.java
+++ Reto2/src/Funciones1.java
@@ -1,4 +1,21 @@
 
 public class Funciones1 {
+	public static double MillasaKm(double num) {
+				return num*0.62137 ;
+	}
+	public static double KmaMillas(double num) {
+		return num/0.62137 ;
+}
+	public static double KgaLibras(double num) {
+		return num*0.4536 ;
+}
+	public static double LibrasaKg(double num) {
+		return num/0.4536 ;
+}
+	
+
+
+
+
 
 }
