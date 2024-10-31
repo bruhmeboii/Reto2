6507f039e6d4c0514e84c099253d7ec7649d1e92
bruhmeboii <dak3rmann@gmail.com>
2024-10-31 09:33:25 +0100

    a

--- Reto2/src/util/FuncionesJava.java
+++ Reto2/src/util/FuncionesJava.java
@@ -1,34 +1,35 @@
 package util;
 
 import java.util.Scanner;
+
 public class FuncionesJava {
 
-	
-		public static int dimeEntero(String texto, Scanner sc) {
-			do {
-				try {
+	public static int dimeEntero(String texto, Scanner sc) {
+		do {
+			try {
 
-					System.out.println(texto);
-					String s = sc.nextLine();
-					int num = Integer.parseInt(s);
-					return num;
-				} catch (Exception ex) {
-					System.out.println("Formato Incorrecto");
-				}
-			} while (true);
-			
-		}
-		
-		public static double dimeDouble(String texto, Scanner sc) {
-			do {
-				try {
+				System.out.println(texto);
+				String s = sc.nextLine();
+				int num = Integer.parseInt(s);
+				return num;
+			} catch (Exception ex) {
+				System.out.println("Formato Incorrecto");
+			}
+		} while (true);
 
-					System.out.println(texto);
-					String s = sc.nextLine();
-					double num = Double.parseDouble(s);
-					return num;
-				} catch (Exception ex) {
-					System.out.println("Formato Incorrecto");
-				}
-			} while (true);
-		}
+	}
+
+	public static double dimeDouble(String texto, Scanner sc) {
+		do {
+			try {
+
+				System.out.println(texto);
+				String s = sc.nextLine();
+				double num = Double.parseDouble(s);
+				return num;
+			} catch (Exception ex) {
+				System.out.println("Formato Incorrecto");
+			}
+		} while (true);
+	}
+}
