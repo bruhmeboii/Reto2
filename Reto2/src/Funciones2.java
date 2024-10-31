a1921eb1d0284e6d875910f31d753c56d315781d
DAM1 <DAM1@aula11-pc20>
2024-10-31 09:48:37 +0100

    Funciones 2 hechas

--- Reto2/src/Funciones2.java
+++ Reto2/src/Funciones2.java
@@ -1,4 +1,35 @@
 
+
+
+
+
 public class Funciones2 {
 
+
+	public static double TemperaturCF (double t) {
+
+		
+double resultado = (t * 9/5) + 32 ;
+
+	return resultado ;}
+		    
+	
+	public static double TemperaturaFC (double t ) {
+	
+	
+	
+	double resultado = (t - 32) * 5/9 ;
+return resultado ;
+	}
+	
+	
+public static double VolumenGL (double v) {
+	double resultado = 0.264172 * v;
+	return resultado ;
 }
+
+public static double VolumenLG (double v) {
+	double resultado = v * 3.785 ;
+	return resultado;
+}}
+
