aa17d70cc6a45e63690b732ed5ed4ea585524744
Alejillo478 <alejopv478@gmail.com>
2024-10-31 09:38:50 +0100

    Funciones 3, efectivo y area

---
+++ Reto2/src/Funciones3.java
@@ -0,0 +1,39 @@
+
+public class Funciones3 {
+
+	public static void main(String[] args) {
+
+		
+		
+	}
+
+	public static double EfectivoED (double dinero)
+	{
+		double n=dinero*1.09;
+		return n;
+		
+	}
+	public static double EfectivoDE (double dinero)
+	{
+		double n=dinero*0.92;
+		return n;
+		
+	}
+	
+	public static double AreaYM (double area)
+	{
+		double n=area*0.836127;
+		return n;
+		
+	}
+	public static double AreaMY (double area)
+	{
+		double n=area*1.19599;
+		return n;
+		
+	}
+	
+	
+	
+	
+}
